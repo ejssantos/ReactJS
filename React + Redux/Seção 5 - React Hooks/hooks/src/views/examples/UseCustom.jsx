b71f3d052b958999dfdfadefb0fe2d15ba1d2e5c
Eduardo José Silva dos Santos <ejssantos@hotmail.com>
2023-07-19 18:43:00 -0300

    Implementação inicial de caso de uso de hook personalizado.

--- React + Redux/Seção 5 - React Hooks/hooks/src/views/examples/UseCustom.jsx
+++ React + Redux/Seção 5 - React Hooks/hooks/src/views/examples/UseCustom.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
+import { useCounter } from '../../hooks/useCounter';
 
 const UseRef = (props) => {
+
+    //Caso não queira informar um valor, poderá declarar conforme instrução abaixo:
+    //const[count, , dec] = useCounter();
+    const[count, inc, dec] = useCounter(10);
+
     return (
         <div className="UseCustom">
             <PageTitle
                 title="Seu Hook"
                 subtitle="Vamos aprender como criar o nosso próprio Hook!"
             />
+
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button className="btn" onClick={() => dec()}>-1</button>
+                    <button className="btn" onClick={() => inc()}>+1</button>
+                </div>
+            </div>
+
+            {/*fecth('http://files.cod3r.com.br/curso-react/estados.json').then(resp => resp.json()).then(json => console.log(json));*/}
         </div>
     )
 }
